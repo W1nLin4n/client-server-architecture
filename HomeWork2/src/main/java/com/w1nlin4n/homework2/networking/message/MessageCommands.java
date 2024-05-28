ac2d30535e0f361892655908dd359653f3d25d5b
W1nLin4n <grechkaartema@gmail.com>
2024-05-28 14:44:49 +0300

    Added enum for all commands used in messages

---
+++ HomeWork2/src/main/java/com/w1nlin4n/homework2/networking/message/MessageCommands.java
@@ -0,0 +1,10 @@
+package com.w1nlin4n.homework2.networking.message;
+
+public enum MessageCommands {
+    GET_PRODUCT_AMOUNT,
+    REMOVE_PRODUCT_AMOUNT,
+    ADD_PRODUCT_AMOUNT,
+    ADD_PRODUCT_GROUP,
+    ADD_PRODUCT_TO_GROUP,
+    SET_PRODUCT_PRICE
+}
