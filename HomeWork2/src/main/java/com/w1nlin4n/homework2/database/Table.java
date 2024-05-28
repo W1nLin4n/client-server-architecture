82f894e559673c6970db2bada0e23f00c559af0d
W1nLin4n <grechkaartema@gmail.com>
2024-05-29 01:11:03 +0300

    Added simple database framework

---
+++ HomeWork2/src/main/java/com/w1nlin4n/homework2/database/Table.java
@@ -0,0 +1,39 @@
+package com.w1nlin4n.homework2.database;
+
+import com.w1nlin4n.homework2.exceptions.DatabaseException;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.HashMap;
+
+@Data
+@AllArgsConstructor
+public class Table {
+    private final String tableName;
+    private final HashMap<Object, Row> rows;
+
+    public void insertRow(Row row) {
+        if(rows.containsKey(row.getId())) {
+            throw new DatabaseException("Duplicate primary key", null);
+        }
+        rows.put(row.getId(), row);
+    }
+
+    public void deleteRow(Object id) {
+        if(!rows.containsKey(id)) {
+            throw new DatabaseException("Primary key not found", null);
+        }
+        rows.remove(id);
+    }
+
+    public Row getRow(Object id) {
+        return rows.get(id);
+    }
+
+    public void updateRow(Row row) {
+        if(!rows.containsKey(row.getId())) {
+            throw new DatabaseException("Primary key not found", null);
+        }
+        rows.put(row.getId(), row);
+    }
+}
