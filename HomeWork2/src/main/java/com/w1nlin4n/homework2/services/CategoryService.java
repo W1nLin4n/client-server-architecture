1d68a1eae2d30a0a2bb88569d518cbca4136b77c
W1nLin4n <grechkaartema@gmail.com>
2024-05-29 15:26:12 +0300

    Added product and category services

---
+++ HomeWork2/src/main/java/com/w1nlin4n/homework2/services/CategoryService.java
@@ -0,0 +1,53 @@
+package com.w1nlin4n.homework2.services;
+
+import com.w1nlin4n.homework2.database.ProductsDB;
+import com.w1nlin4n.homework2.dto.AddProductToCategoryDto;
+import com.w1nlin4n.homework2.dto.CategoryDto;
+import com.w1nlin4n.homework2.dto.ProductDto;
+import com.w1nlin4n.homework2.entities.Category;
+import com.w1nlin4n.homework2.entities.Product;
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+public class CategoryService {
+    private final ProductsDB productsDB;
+
+    public void createCategory(CategoryDto category) {
+        productsDB.createCategory(category.toCategory());
+    }
+
+    public CategoryDto getCategory(String categoryName) {
+        return CategoryDto.fromCategory(productsDB.getCategory(categoryName));
+    }
+
+    public void updateCategory(CategoryDto category) {
+        productsDB.updateCategory(category.toCategory());
+    }
+
+    public void deleteCategory(String categoryName) {
+        productsDB.deleteCategory(categoryName);
+    }
+
+    public void addProductToCategory(AddProductToCategoryDto addProductToCategoryDto) {
+        productsDB.addProductToCategory(addProductToCategoryDto.getProductName(), addProductToCategoryDto.getCategoryName());
+    }
+
+    public List<ProductDto> getAllProductsFromCategory(String categoryName) {
+        List<ProductDto> result = new ArrayList<>();
+        for (Product product : productsDB.getAllProductsFromCategory(categoryName)) {
+            result.add(ProductDto.fromProduct(product));
+        }
+        return result;
+    }
+
+    public List<CategoryDto> getAllCategories() {
+        List<CategoryDto> result = new ArrayList<>();
+        for (Category category : productsDB.getAllCategories()) {
+            result.add(CategoryDto.fromCategory(category));
+        }
+        return result;
+    }
+}
---
+++ HomeWork2/src/main/java/com/w1nlin4n/homework2/services/ProductService.java
@@ -0,0 +1,66 @@
+package com.w1nlin4n.homework2.services;
+
+import com.w1nlin4n.homework2.database.ProductsDB;
+import com.w1nlin4n.homework2.dto.ProductAmountChangeDto;
+import com.w1nlin4n.homework2.dto.ProductDto;
+import com.w1nlin4n.homework2.entities.Product;
+import com.w1nlin4n.homework2.exceptions.LogicException;
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+public class ProductService {
+    private final ProductsDB productsDB;
+
+    public void createProduct(ProductDto product) {
+        if (product.getAmount() < 0)
+            throw new LogicException("Product amount cannot be negative", null);
+        if (product.getPrice() < 0)
+            throw new LogicException("Product price cannot be negative", null);
+        productsDB.createProduct(product.toProduct());
+    }
+
+    public ProductDto getProduct(String productName) {
+        return ProductDto.fromProduct(productsDB.getProduct(productName));
+    }
+
+    public void updateProduct(ProductDto product) {
+        if (product.getAmount() < 0)
+            throw new LogicException("Product amount cannot be negative", null);
+        if (product.getPrice() < 0)
+            throw new LogicException("Product price cannot be negative", null);
+        productsDB.updateProduct(product.toProduct());
+    }
+
+    public void deleteProduct(String productName) {
+        productsDB.deleteProduct(productName);
+    }
+
+    public void addAmountToProduct(ProductAmountChangeDto productAmountChangeDto) {
+        synchronized (productsDB) {
+            Product product = productsDB.getProduct(productAmountChangeDto.getProductName());
+            product.setAmount(product.getAmount() + productAmountChangeDto.getProductAmount());
+            productsDB.updateProduct(product);
+        }
+    }
+
+    public void removeAmountFromProduct(ProductAmountChangeDto productAmountChangeDto) {
+        synchronized (productsDB) {
+            Product product = productsDB.getProduct(productAmountChangeDto.getProductName());
+            product.setAmount(product.getAmount() - productAmountChangeDto.getProductAmount());
+            if (product.getAmount() < 0)
+                throw new LogicException("Product amount cannot be negative", null);
+            productsDB.updateProduct(product);
+        }
+    }
+
+    public List<ProductDto> getAllProducts() {
+        List<ProductDto> result = new ArrayList<>();
+        for (Product product : productsDB.getAllProducts()) {
+            result.add(ProductDto.fromProduct(product));
+        }
+        return result;
+    }
+}
