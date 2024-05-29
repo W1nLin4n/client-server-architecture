9d924fc9a6b105b4e686ecd0e87baa82f2411a7f
W1nLin4n <grechkaartema@gmail.com>
2024-05-29 16:38:49 +0300

    Added converter from integer to enum values

--- HomeWork2/src/main/java/com/w1nlin4n/homework2/networking/message/MessageCommand.java
+++ HomeWork2/src/main/java/com/w1nlin4n/homework2/networking/message/MessageCommand.java
@@ -16,5 +16,27 @@ public enum MessageCommand {
     DELETE_PRODUCT_GROUP,
     ADD_PRODUCT_TO_GROUP,
     GET_PRODUCTS_FROM_GROUP,
-    GET_ALL_PRODUCT_GROUPS
+    GET_ALL_PRODUCT_GROUPS;
+
+    public static MessageCommand getCommand(int command) {
+        return switch (command) {
+            case 0 -> INFORMATION;
+            case 1 -> ERROR;
+            case 2 -> CREATE_PRODUCT;
+            case 3 -> GET_PRODUCT;
+            case 4 -> UPDATE_PRODUCT;
+            case 5 -> DELETE_PRODUCT;
+            case 6 -> ADD_PRODUCT_AMOUNT;
+            case 7 -> REMOVE_PRODUCT_AMOUNT;
+            case 8 -> GET_ALL_PRODUCTS;
+            case 9 -> CREATE_PRODUCT_GROUP;
+            case 10 -> GET_PRODUCT_GROUP;
+            case 11 -> UPDATE_PRODUCT_GROUP;
+            case 12 -> DELETE_PRODUCT_GROUP;
+            case 13 -> ADD_PRODUCT_TO_GROUP;
+            case 14 -> GET_PRODUCTS_FROM_GROUP;
+            case 15 -> GET_ALL_PRODUCT_GROUPS;
+            default -> null;
+        };
+    }
 }
