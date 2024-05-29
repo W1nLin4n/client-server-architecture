01c865d074558e42e869ec22040b3a218c5f9b82
W1nLin4n <grechkaartema@gmail.com>
2024-05-29 15:25:28 +0300

    Added LogicException to throw when business logic does not allow operation

---
+++ HomeWork2/src/main/java/com/w1nlin4n/homework2/exceptions/LogicException.java
@@ -0,0 +1,7 @@
+package com.w1nlin4n.homework2.exceptions;
+
+public class LogicException extends RuntimeException {
+    public LogicException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
