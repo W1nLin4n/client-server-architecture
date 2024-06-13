ec27e589de14bd45f984a333c79f9c6eced9a81b
W1nLin4n <grechkaartema@gmail.com>
2024-06-13 22:17:50 +0300

    Updated MessageCommand by adding CLOSE_CONNECTION and updating getCommand method

--- Practice3/src/main/java/com/w1nlin4n/practice3/networking/message/MessageCommand.java
+++ Practice3/src/main/java/com/w1nlin4n/practice3/networking/message/MessageCommand.java
@@ -1,6 +1,7 @@
 package com.w1nlin4n.practice3.networking.message;
 
 public enum MessageCommand {
+    CLOSE_CONNECTION,
     INFORMATION,
     ERROR,
     CREATE_PRODUCT,
@@ -19,24 +20,6 @@ public enum MessageCommand {
     GET_ALL_PRODUCT_GROUPS;
 
     public static MessageCommand getCommand(int command) {
-        return switch (command) {
-            case 0 -> INFORMATION;
-            case 1 -> ERROR;
-            case 2 -> CREATE_PRODUCT;
-            case 3 -> GET_PRODUCT;
-            case 4 -> UPDATE_PRODUCT;
-            case 5 -> DELETE_PRODUCT;
-            case 6 -> ADD_PRODUCT_AMOUNT;
-            case 7 -> REMOVE_PRODUCT_AMOUNT;
-            case 8 -> GET_ALL_PRODUCTS;
-            case 9 -> CREATE_PRODUCT_GROUP;
-            case 10 -> GET_PRODUCT_GROUP;
-            case 11 -> UPDATE_PRODUCT_GROUP;
-            case 12 -> DELETE_PRODUCT_GROUP;
-            case 13 -> ADD_PRODUCT_TO_GROUP;
-            case 14 -> GET_PRODUCTS_FROM_GROUP;
-            case 15 -> GET_ALL_PRODUCT_GROUPS;
-            default -> null;
-        };
+        return MessageCommand.values()[command];
     }
 }
