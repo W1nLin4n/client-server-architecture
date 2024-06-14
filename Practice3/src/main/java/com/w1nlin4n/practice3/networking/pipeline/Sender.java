505e487e25a5a28959af6cb05f1c8d4dd320e272
W1nLin4n <grechkaartema@gmail.com>
2024-06-14 17:55:57 +0300

    Updated sender to send packet over network

--- Practice3/src/main/java/com/w1nlin4n/practice3/networking/pipeline/Sender.java
+++ Practice3/src/main/java/com/w1nlin4n/practice3/networking/pipeline/Sender.java
@@ -1,9 +1,21 @@
 package com.w1nlin4n.practice3.networking.pipeline;
 
-import java.util.Arrays;
+import com.w1nlin4n.practice3.networking.packet.Packet;
+import com.w1nlin4n.practice3.serialization.Serializer;
+import lombok.AllArgsConstructor;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+@AllArgsConstructor
 public class Sender {
-    public void sendPacket(byte[] packet, byte address) {
-        System.out.println("Sending to " + address + " : " + Arrays.toString(packet));
+    Serializer<Packet> serializer;
+
+    public void sendPacket(Packet packet, SocketChannel channel) throws IOException {
+        ByteBuffer buffer = ByteBuffer.wrap(serializer.serialize(packet));
+        while (buffer.hasRemaining()) {
+            channel.write(buffer);
+        }
     }
 }
