07da46d1802bc4f6ce80e3b4cc2fb00bca0d3e15
W1nLin4n <grechkaartema@gmail.com>
2024-06-28 02:49:08 +0300

    Updated endpoint annotation to contain properties used in HTTP request processing

--- Practice5/src/main/java/com/w1nlin4n/practice5/controllers/CategoryController.java
+++ Practice5/src/main/java/com/w1nlin4n/practice5/controllers/CategoryController.java
@@ -16,6 +16,7 @@ public class CategoryController {
 
     @Endpoint(command = MessageCommand.CREATE_PRODUCT_GROUP, dto = CategoryDto.class)
     public void createCategory(CategoryDto category) {
+    @Endpoint(path = "/category", method = HttpMethod.POST, onSuccess = HttpCode.CREATED)
         categoryService.createCategory(category);
     }
 
@@ -27,24 +28,29 @@ public class CategoryController {
     @Endpoint(command = MessageCommand.UPDATE_PRODUCT_GROUP, dto = CategoryDto.class)
     public void updateCategory(CategoryDto category) {
         categoryService.updateCategory(category);
+    @Endpoint(path = "/category/{id}", method = HttpMethod.GET, onFailure = HttpCode.NOT_FOUND)
     }
 
     @Endpoint(command = MessageCommand.DELETE_PRODUCT_GROUP, dto = String.class)
     public void deleteCategory(String categoryName) {
         categoryService.deleteCategory(categoryName);
+    @Endpoint(path = "/category/{id}", method = HttpMethod.PUT)
     }
 
     @Endpoint(command = MessageCommand.ADD_PRODUCT_TO_GROUP, dto = AddProductToCategoryDto.class)
     public void addProductToCategory(AddProductToCategoryDto addProductToCategoryDto) {
         categoryService.addProductToCategory(addProductToCategoryDto);
+    @Endpoint(path = "/category/{id}", method = HttpMethod.DELETE)
     }
 
     @Endpoint(command = MessageCommand.GET_PRODUCTS_FROM_GROUP, dto = String.class)
     public List<ProductDto> getAllProductsFromCategory(String categoryName) {
         return categoryService.getAllProductsFromCategory(categoryName);
+    @Endpoint(path = "/category/{id}/products", method = HttpMethod.GET)
     }
 
     @Endpoint(command = MessageCommand.GET_ALL_PRODUCT_GROUPS)
+    @Endpoint(path = "/category/all", method = HttpMethod.GET)
     public List<CategoryDto> getAllCategories() {
         return categoryService.getAllCategories();
     }
--- Practice5/src/main/java/com/w1nlin4n/practice5/controllers/ProductController.java
+++ Practice5/src/main/java/com/w1nlin4n/practice5/controllers/ProductController.java
@@ -15,35 +15,42 @@ public class ProductController {
 
     @Endpoint(command = MessageCommand.CREATE_PRODUCT, dto = ProductDto.class)
     public void createProduct(ProductDto product) {
+    @Endpoint(path = "/product", method = HttpMethod.POST, onSuccess = HttpCode.CREATED)
         productService.createProduct(product);
     }
 
     @Endpoint(command = MessageCommand.GET_PRODUCT, dto = String.class)
     public ProductDto getProduct(String productName) {
         return productService.getProduct(productName);
+    @Endpoint(path = "/product/{id}", method = HttpMethod.GET, onFailure = HttpCode.NOT_FOUND)
     }
 
     @Endpoint(command = MessageCommand.UPDATE_PRODUCT, dto = ProductDto.class)
     public void updateProduct(ProductDto product) {
         productService.updateProduct(product);
+    @Endpoint(path = "/product/{id}", method = HttpMethod.PUT)
     }
 
     @Endpoint(command = MessageCommand.DELETE_PRODUCT, dto = String.class)
     public void deleteProduct(String productName) {
         productService.deleteProduct(productName);
+    @Endpoint(path = "/product/{id}", method = HttpMethod.DELETE)
     }
 
     @Endpoint(command = MessageCommand.ADD_PRODUCT_AMOUNT, dto = ProductAmountChangeDto.class)
     public void addAmountToProduct(ProductAmountChangeDto productAmountChangeDto) {
         productService.addAmountToProduct(productAmountChangeDto);
+    @Endpoint(path = "/product/{id}/add/{amount}", method = HttpMethod.PUT)
     }
 
     @Endpoint(command = MessageCommand.REMOVE_PRODUCT_AMOUNT, dto = ProductAmountChangeDto.class)
     public void removeAmountFromProduct(ProductAmountChangeDto productAmountChangeDto) {
         productService.removeAmountFromProduct(productAmountChangeDto);
+    @Endpoint(path = "/product/{id}/remove/{amount}", method = HttpMethod.PUT)
     }
 
     @Endpoint(command = MessageCommand.GET_ALL_PRODUCTS, dto = ProductDto.class)
+    @Endpoint(path = "/product/all", method = HttpMethod.GET)
     public List<ProductDto> getAllProducts() {
         return productService.getAllProducts();
     }
