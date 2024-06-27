64b0b1f23d6a6d88206b98a5c7f6fbbcdf56aba3
W1nLin4n <grechkaartema@gmail.com>
2024-06-28 02:51:36 +0300

    Added security annotation to secure endpoints

--- Practice5/src/main/java/com/w1nlin4n/practice5/controllers/CategoryController.java
+++ Practice5/src/main/java/com/w1nlin4n/practice5/controllers/CategoryController.java
@@ -1,6 +1,7 @@
 package com.w1nlin4n.practice5.controllers;
 
 import com.w1nlin4n.practice5.dto.AddProductToCategoryDto;
+import com.w1nlin4n.practice5.controllers.security.Security;
 import com.w1nlin4n.practice5.dto.CategoryDto;
 import com.w1nlin4n.practice5.dto.ProductDto;
 import com.w1nlin4n.practice5.networking.message.MessageCommand;
@@ -14,42 +15,28 @@ import java.util.List;
 public class CategoryController {
     private final CategoryService categoryService;
 
-    @Endpoint(command = MessageCommand.CREATE_PRODUCT_GROUP, dto = CategoryDto.class)
-    public void createCategory(CategoryDto category) {
+    @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/category", method = HttpMethod.POST, onSuccess = HttpCode.CREATED)
         categoryService.createCategory(category);
     }
 
-    @Endpoint(command = MessageCommand.GET_PRODUCT_GROUP, dto = String.class)
-    public CategoryDto getCategory(String categoryName) {
-        return categoryService.getCategory(categoryName);
-    }
-
-    @Endpoint(command = MessageCommand.UPDATE_PRODUCT_GROUP, dto = CategoryDto.class)
-    public void updateCategory(CategoryDto category) {
-        categoryService.updateCategory(category);
+    @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/category/{id}", method = HttpMethod.GET, onFailure = HttpCode.NOT_FOUND)
     }
 
-    @Endpoint(command = MessageCommand.DELETE_PRODUCT_GROUP, dto = String.class)
-    public void deleteCategory(String categoryName) {
-        categoryService.deleteCategory(categoryName);
+    @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/category/{id}", method = HttpMethod.PUT)
     }
 
-    @Endpoint(command = MessageCommand.ADD_PRODUCT_TO_GROUP, dto = AddProductToCategoryDto.class)
-    public void addProductToCategory(AddProductToCategoryDto addProductToCategoryDto) {
-        categoryService.addProductToCategory(addProductToCategoryDto);
+    @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/category/{id}", method = HttpMethod.DELETE)
     }
 
-    @Endpoint(command = MessageCommand.GET_PRODUCTS_FROM_GROUP, dto = String.class)
-    public List<ProductDto> getAllProductsFromCategory(String categoryName) {
-        return categoryService.getAllProductsFromCategory(categoryName);
+    @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/category/{id}/products", method = HttpMethod.GET)
     }
 
-    @Endpoint(command = MessageCommand.GET_ALL_PRODUCT_GROUPS)
+    @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/category/all", method = HttpMethod.GET)
     public List<CategoryDto> getAllCategories() {
         return categoryService.getAllCategories();
--- Practice5/src/main/java/com/w1nlin4n/practice5/controllers/ProductController.java
+++ Practice5/src/main/java/com/w1nlin4n/practice5/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package com.w1nlin4n.practice5.controllers;
 
 import com.w1nlin4n.practice5.dto.ProductAmountChangeDto;
+import com.w1nlin4n.practice5.controllers.security.Security;
 import com.w1nlin4n.practice5.dto.ProductDto;
 import com.w1nlin4n.practice5.networking.message.MessageCommand;
 import com.w1nlin4n.practice5.services.ProductService;
@@ -13,43 +14,32 @@ import java.util.List;
 public class ProductController {
     private final ProductService productService;
 
-    @Endpoint(command = MessageCommand.CREATE_PRODUCT, dto = ProductDto.class)
-    public void createProduct(ProductDto product) {
+    @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/product", method = HttpMethod.POST, onSuccess = HttpCode.CREATED)
         productService.createProduct(product);
     }
 
-    @Endpoint(command = MessageCommand.GET_PRODUCT, dto = String.class)
-    public ProductDto getProduct(String productName) {
-        return productService.getProduct(productName);
+    @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/product/{id}", method = HttpMethod.GET, onFailure = HttpCode.NOT_FOUND)
     }
 
-    @Endpoint(command = MessageCommand.UPDATE_PRODUCT, dto = ProductDto.class)
-    public void updateProduct(ProductDto product) {
-        productService.updateProduct(product);
+    @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/product/{id}", method = HttpMethod.PUT)
     }
 
-    @Endpoint(command = MessageCommand.DELETE_PRODUCT, dto = String.class)
-    public void deleteProduct(String productName) {
-        productService.deleteProduct(productName);
+    @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/product/{id}", method = HttpMethod.DELETE)
     }
 
-    @Endpoint(command = MessageCommand.ADD_PRODUCT_AMOUNT, dto = ProductAmountChangeDto.class)
-    public void addAmountToProduct(ProductAmountChangeDto productAmountChangeDto) {
-        productService.addAmountToProduct(productAmountChangeDto);
+    @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/product/{id}/add/{amount}", method = HttpMethod.PUT)
     }
 
-    @Endpoint(command = MessageCommand.REMOVE_PRODUCT_AMOUNT, dto = ProductAmountChangeDto.class)
-    public void removeAmountFromProduct(ProductAmountChangeDto productAmountChangeDto) {
-        productService.removeAmountFromProduct(productAmountChangeDto);
+    @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/product/{id}/remove/{amount}", method = HttpMethod.PUT)
     }
 
-    @Endpoint(command = MessageCommand.GET_ALL_PRODUCTS, dto = ProductDto.class)
+    @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/product/all", method = HttpMethod.GET)
     public List<ProductDto> getAllProducts() {
         return productService.getAllProducts();
