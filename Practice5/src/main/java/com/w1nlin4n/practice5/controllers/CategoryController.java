a35d5c61ce8b2904b71652135c12876a33d699ed
W1nLin4n <grechkaartema@gmail.com>
2024-06-28 03:00:04 +0300

    Updated controllers to use new services methods and tables structure with integer id

--- Practice5/src/main/java/com/w1nlin4n/practice5/controllers/CategoryController.java
+++ Practice5/src/main/java/com/w1nlin4n/practice5/controllers/CategoryController.java
@@ -1,12 +1,14 @@
 package com.w1nlin4n.practice5.controllers;
 
-import com.w1nlin4n.practice5.dto.AddProductToCategoryDto;
+import com.w1nlin4n.practice5.controllers.endpoint.Endpoint;
+import com.w1nlin4n.practice5.networking.HttpCode;
+import com.w1nlin4n.practice5.networking.HttpMethod;
 import com.w1nlin4n.practice5.controllers.endpoint.params.Body;
 import com.w1nlin4n.practice5.controllers.endpoint.params.Path;
+import com.w1nlin4n.practice5.controllers.security.AccessLevel;
 import com.w1nlin4n.practice5.controllers.security.Security;
 import com.w1nlin4n.practice5.dto.CategoryDto;
 import com.w1nlin4n.practice5.dto.ProductDto;
-import com.w1nlin4n.practice5.networking.message.MessageCommand;
 import com.w1nlin4n.practice5.services.CategoryService;
 import lombok.AllArgsConstructor;
 
@@ -26,21 +28,25 @@ public class CategoryController {
     @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/category/{id}", method = HttpMethod.GET, onFailure = HttpCode.NOT_FOUND)
     public CategoryDto getCategory(@Path(name = "id") Integer id) {
+        return categoryService.getCategory(id);
     }
 
     @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/category/{id}", method = HttpMethod.PUT)
     public void updateCategory(@Path(name = "id") Integer id, @Body CategoryDto category) {
+        categoryService.updateCategory(id, category);
     }
 
     @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/category/{id}", method = HttpMethod.DELETE)
     public void deleteCategory(@Path(name = "id") Integer id) {
+        categoryService.deleteCategory(id);
     }
 
     @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/category/{id}/products", method = HttpMethod.GET)
     public List<ProductDto> getAllProductsFromCategory(@Path(name = "id") Integer id) {
+        return categoryService.getAllProductsFromCategory(id);
     }
 
     @Security(level = {AccessLevel.USER})
--- Practice5/src/main/java/com/w1nlin4n/practice5/controllers/ProductController.java
+++ Practice5/src/main/java/com/w1nlin4n/practice5/controllers/ProductController.java
@@ -1,11 +1,13 @@
 package com.w1nlin4n.practice5.controllers;
 
-import com.w1nlin4n.practice5.dto.ProductAmountChangeDto;
+import com.w1nlin4n.practice5.controllers.endpoint.Endpoint;
+import com.w1nlin4n.practice5.networking.HttpCode;
+import com.w1nlin4n.practice5.networking.HttpMethod;
 import com.w1nlin4n.practice5.controllers.endpoint.params.Body;
 import com.w1nlin4n.practice5.controllers.endpoint.params.Path;
+import com.w1nlin4n.practice5.controllers.security.AccessLevel;
 import com.w1nlin4n.practice5.controllers.security.Security;
 import com.w1nlin4n.practice5.dto.ProductDto;
-import com.w1nlin4n.practice5.networking.message.MessageCommand;
 import com.w1nlin4n.practice5.services.ProductService;
 import lombok.AllArgsConstructor;
 
@@ -25,26 +27,31 @@ public class ProductController {
     @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/product/{id}", method = HttpMethod.GET, onFailure = HttpCode.NOT_FOUND)
     public ProductDto getProduct(@Path(name = "id") Integer id) {
+        return productService.getProduct(id);
     }
 
     @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/product/{id}", method = HttpMethod.PUT)
     public void updateProduct(@Path(name = "id") Integer id, @Body ProductDto product) {
+        productService.updateProduct(id, product);
     }
 
     @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/product/{id}", method = HttpMethod.DELETE)
     public void deleteProduct(@Path(name = "id") Integer id) {
+        productService.deleteProduct(id);
     }
 
     @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/product/{id}/add/{amount}", method = HttpMethod.PUT)
     public void addAmountToProduct(@Path(name = "id") Integer id, @Path(name = "amount") Integer amount) {
+        productService.addAmountToProduct(id, amount);
     }
 
     @Security(level = {AccessLevel.USER})
     @Endpoint(path = "/product/{id}/remove/{amount}", method = HttpMethod.PUT)
     public void removeAmountFromProduct(@Path(name = "id") Integer id, @Path(name = "amount") Integer amount) {
+        productService.removeAmountFromProduct(id, amount);
     }
 
     @Security(level = {AccessLevel.USER})
